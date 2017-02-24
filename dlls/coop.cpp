6a131615c76645901c1b38256a1732966bc50474
mittorn <mittorn@sibmail.com>
2017-02-24 09:22:26 +0000

    Fix build

--- dlls/coop.cpp
+++ dlls/coop.cpp
@@ -821,8 +821,8 @@ static char *strrstr(const char *x, const char *y) {
 char *prev = NULL;
 char *next;
 if (*y == '\0')
-return strchr(x, '\0');
-while ((next = strstr(x, y)) != NULL) {
+return (char*)strchr(x, '\0');
+while ((next = (char*)strstr(x, y)) != NULL) {
 prev = next;
 x = next + 1;
 }
