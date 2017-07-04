c4820ad0df7bda569275452ad8f25c60d00301f9
Night Owl <sepulkarium45@yahoo.com>
2017-07-05 03:12:53 +0500

    Merge https://github.com/SamVanheer/HLEnhanced/commit/89efd616d6aed0304db5bf63bfecd311c1cedbdd

--- dlls/subs.cpp
+++ dlls/subs.cpp
@@ -107,6 +107,13 @@ void CBaseEntity::UpdateOnRemove( void )
 
 	if( pev->globalname )
 		gGlobalState.EntitySetState( pev->globalname, GLOBAL_DEAD );
+
+	// tell owner ( if any ) that we're dead.This is mostly for MonsterMaker functionality.
+	//Killtarget didn't do this before, so the counter broke. - Solokiller
+	if( CBaseEntity* pOwner = pev->owner ? Instance( pev->owner ) : 0 )
+	{
+		pOwner->DeathNotice( this );
+	}
 }
 
 // Convenient way to delay removing oneself
