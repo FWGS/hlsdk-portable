33ba064b88d9b0e818009049aef13f53bd16ae20
Night Owl <sepulkarium45@yahoo.com>
2017-07-05 17:53:04 +0500

    Merge https://github.com/SamVanheer/HLEnhanced/commit/4dfa6b8710a0a30761b298a02bee21c65f056327

--- dlls/crowbar.cpp
+++ dlls/crowbar.cpp
@@ -183,9 +183,12 @@ int CCrowbar::Swing( int fFirst )
 		}
 	}
 #endif
-	PLAYBACK_EVENT_FULL( FEV_NOTHOST, m_pPlayer->edict(), m_usCrowbar, 
-	0.0, (float *)&g_vecZero, (float *)&g_vecZero, 0, 0, 0,
-	0, 0, 0 );
+	if( fFirst )
+	{
+		PLAYBACK_EVENT_FULL( FEV_NOTHOST, m_pPlayer->edict(), m_usCrowbar, 
+		0.0, (float *)&g_vecZero, (float *)&g_vecZero, 0, 0, 0,
+		0, 0, 0 );
+	}
 
 	if( tr.flFraction >= 1.0 )
 	{
