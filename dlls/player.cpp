38b223651533fd0ab984147f906e2fe174683eb9
Night Owl <sepulkarium45@yahoo.com>
2017-07-17 12:01:53 +0500

    Merge https://github.com/LevShisterov/BugfixedHL/commit/ee1ec9d64f96ecce0ab144cd86b246f5e7b00555

--- dlls/player.cpp
+++ dlls/player.cpp
@@ -2583,11 +2583,10 @@ void CBasePlayer::PostThink()
 	CheckPowerups( pev );
 
 	UpdatePlayerSound();
-
+pt_end:
 	// Track button info so we can detect 'pressed' and 'released' buttons next frame
 	m_afButtonLast = pev->button;
 
-pt_end:
 #if defined( CLIENT_WEAPONS )
 	// Decay timers on weapons
 	// go through all of the weapons and make a list of the ones to pack
@@ -4059,6 +4058,52 @@ void CBasePlayer::UpdateClientData( void )
 	{
 		if( m_rgpPlayerItems[i] )  // each item updates it's successors
 			m_rgpPlayerItems[i]->UpdateClientData( this );
+	}
+
+	if( m_pClientActiveItem != pPlayer->m_pActiveItem )
+	{
+		if( pPlayer->m_pActiveItem == NULL )
+		{
+			// If no weapon, we have to send update here
+			CBasePlayer *plr;
+			for( int i = 1; i <= gpGlobals->maxClients; i++ )
+			{
+				plr = (CBasePlayer *)UTIL_PlayerByIndex( i );
+				if( !plr || !plr->IsObserver() || plr->m_hObserverTarget != pPlayer )
+					continue;
+
+				MESSAGE_BEGIN( MSG_ONE, gmsgCurWeapon, NULL, plr->pev );
+					WRITE_BYTE( 0 );
+					WRITE_BYTE( 0 );
+					WRITE_BYTE( 0 );
+				MESSAGE_END();
+			}
+
+			MESSAGE_BEGIN( MSG_ONE, gmsgCurWeapon, NULL, pPlayer->pev );
+				WRITE_BYTE( 0 );
+				WRITE_BYTE( 0 );
+				WRITE_BYTE( 0 );
+			MESSAGE_END();
+		}
+		else if( this != pPlayer )
+		{
+			// Special case for spectator
+			CBasePlayerWeapon *gun = (CBasePlayerWeapon *)pPlayer->m_pActiveItem->GetWeaponPtr();
+			if( gun )
+			{
+				int state;
+				if( pPlayer->m_fOnTarget )
+					state = WEAPON_IS_ONTARGET;
+				else
+					state = 1;
+
+				MESSAGE_BEGIN( MSG_ONE, gmsgCurWeapon, NULL, pev );
+					WRITE_BYTE( state );
+					WRITE_BYTE( gun->m_iId );
+					WRITE_BYTE( gun->m_iClip );
+				MESSAGE_END();
+			}
+		}
 	}
 
 	// Cache and client weapon change
