aaf81c770994d179c43b39e4233299752dec7f44
Night Owl <sepulkarium45@yahoo.com>
2017-07-24 16:56:43 +0500

    Merge https://github.com/LevShisterov/BugfixedHL/commit/ef7cecf654ccf7716a5646ff9498928905a266dc

--- pm_shared/pm_math.c
+++ pm_shared/pm_math.c
@@ -239,7 +239,7 @@ float AngleBetweenVectors( const vec3_t v1, const vec3_t v2 )
 	if( !l1 || !l2 )
 		return 0.0f;
 
-	angle = acos( DotProduct( v1, v2 ) ) / ( l1 * l2 );
+	angle = acos( DotProduct( v1, v2 ) / ( l1 * l2 ) );
 	angle = ( angle  * 180.0f ) / M_PI;
 
 	return angle;
