0ac6b569d8945551c143b9fd9652f0ed92000bb8
Night Owl <sepulkarium45@yahoo.com>
2017-11-05 20:07:15 +0500

    Fix zombie soldier health.

--- dlls/zombie.cpp
+++ dlls/zombie.cpp
@@ -250,7 +250,10 @@ void CZombie::Spawn()
 	pev->solid		= SOLID_SLIDEBOX;
 	pev->movetype		= MOVETYPE_STEP;
 	m_bloodColor		= BLOOD_COLOR_GREEN;
-	pev->health		= gSkillData.zombieHealth;
+	if( FClassnameIs( pev, "monster_zombie_soldier" ) )
+		pev->health	= gSkillData.zsoldierHealth;
+	else
+		pev->health	= gSkillData.zombieHealth;
 	pev->view_ofs		= VEC_VIEW;// position of the eyes relative to monster's origin.
 	m_flFieldOfView		= 0.5;// indicates the width of this monster's forward view cone ( as a dotproduct result )
 	m_MonsterState		= MONSTERSTATE_NONE;
