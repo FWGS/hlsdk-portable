f3348046b920f5e30ed7fd795a10ac4236c5b0c3
Night Owl <sepulkarium45@yahoo.com>
2018-01-29 14:07:24 +0500

    Revert "Try fix stuck in moving train on vis04 map." because this commit causes another troubles under GoldSrc.
    This reverts commit 83c0040af73b6f744c124a7519e5956633ad2eb2.

--- dlls/triggers.cpp
+++ dlls/triggers.cpp
@@ -1437,7 +1437,7 @@ void CChangeLevel::UseChangeLevel( CBaseEntity *pActivator, CBaseEntity *pCaller
 
 void CChangeLevel::ChangeLevelNow( CBaseEntity *pActivator )
 {
-	edict_t	*pentLandmark = 0;
+	edict_t	*pentLandmark;
 	//LEVELLIST levels[16];
 
 	ASSERT( !FStrEq( m_szMapName, "" ) );
@@ -1483,9 +1483,7 @@ void CChangeLevel::ChangeLevelNow( CBaseEntity *pActivator )
 	st_szNextSpot[0] = 0;	// Init landmark to NULL
 
 	// look for a landmark entity
-	if( !FStrEq( st_szNextMap, "vis04" ) )
-		pentLandmark = FindLandmark( m_szLandmarkName );
-
+	pentLandmark = FindLandmark( m_szLandmarkName );
 	if( !FNullEnt( pentLandmark ) )
 	{
 		strcpy( st_szNextSpot, m_szLandmarkName );
