8fb5b78fda8b502a820de695d2dfaa46e74a5d12
Night Owl <sepulkarium45@yahoo.com>
2018-01-29 11:38:03 +0500

    Update game description.

--- dlls/client.cpp
+++ dlls/client.cpp
@@ -949,7 +949,7 @@ const char *GetGameDescription()
 	if( g_pGameRules ) // this function may be called before the world has spawned, and the game rules initialized
 		return g_pGameRules->GetGameDescription();
 	else
-		return "Half-Life";
+		return "Half-Life: Visitors";
 }
 
 /*
