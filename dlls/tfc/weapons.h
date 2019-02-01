18ca7f0eb727469ff2e3f5bca8f8d3267239939b
DPT.Velaron <mars873@gmail.com>
2019-02-01 11:38:04 +0200

    Vse zhe vernul.

--- dlls/tfc/weapons.h
+++ dlls/tfc/weapons.h
@@ -393,6 +393,39 @@ public:
 	BOOL m_fRegisteredSound;// whether or not this grenade has issued its DANGER sound to the world sound list yet.
 };
 
+class CCrowbar : public CBasePlayerWeapon
+{
+public:
+	void Spawn( void );
+	void Precache( void );
+	int iItemSlot( void ) { return 1; }
+	void EXPORT SwingAgain( void );
+	void EXPORT Smack( void );
+	int GetItemInfo( ItemInfo *p );
+	int AddToPlayer( CBasePlayer *pPlayer );
+
+	void PrimaryAttack( void );
+	int Swing( int fFirst );
+	BOOL Deploy( void );
+	void Holster( int skiplocal = 0 );
+#ifdef CROWBAR_IDLE_ANIM
+	void WeaponIdle();
+#endif
+	int m_iSwing;
+	TraceResult m_trHit;
+
+	virtual BOOL UseDecrement( void )
+	{ 
+#if defined( CLIENT_WEAPONS )
+		return TRUE;
+#else
+		return FALSE;
+#endif
+	}
+private:
+	unsigned short m_usCrowbar;
+};
+
 class CLaserSpot : public CBaseEntity
 {
 	void Spawn( void );
@@ -422,7 +455,7 @@ enum tfc_axe_e
     AXE_IDLE3
 };
 
-class CTFAxe : public CBasePlayerWeapon //CTFAxe
+class CTFAxe : public CCrowbar
 {
 public:
 	void Spawn(void);
