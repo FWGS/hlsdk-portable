fe601bce06ada41dceca219aa436132a6f231ab5
Roman Chistokhodov <freeslave93@gmail.com>
2019-08-06 11:49:22 +0300

    Give 50 ammo for mp5 like in opfor

--- dlls/weapons.h
+++ dlls/weapons.h
@@ -170,7 +170,7 @@ public:
 // the default amount of ammo that comes with each gun when it spawns
 #define GLOCK_DEFAULT_GIVE			17
 #define PYTHON_DEFAULT_GIVE			6
-#define MP5_DEFAULT_GIVE			25
+#define MP5_DEFAULT_GIVE			50
 #define MP5_DEFAULT_AMMO			25
 #define MP5_M203_DEFAULT_GIVE		0
 #define SHOTGUN_DEFAULT_GIVE		12
