c96794aa2dfd3120b8a133555c7192a9e527edc1
Roman Chistokhodov <freeslave93@gmail.com>
2019-10-16 01:51:48 +0300

    Place crowbar delay fix under a macro

--- dlls/crowbar.cpp
+++ dlls/crowbar.cpp
@@ -330,7 +330,11 @@ int CCrowbar::Swing( int fFirst )
 		SetThink( &CCrowbar::Smack );
 		pev->nextthink = UTIL_WeaponTimeBase() + 0.2;
 #endif
+#if CROWBAR_DELAY_FIX
 		m_flNextPrimaryAttack = UTIL_WeaponTimeBase() + 0.25;
+#else
+		m_flNextPrimaryAttack = GetNextAttackDelay( 0.25 );
+#endif
 	}
 #ifdef CROWBAR_IDLE_ANIM
 	m_flTimeWeaponIdle = UTIL_WeaponTimeBase() + UTIL_SharedRandomFloat( m_pPlayer->random_seed, 10, 15 );
