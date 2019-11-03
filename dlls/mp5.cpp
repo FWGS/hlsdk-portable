46f5f21392271081c714f5d10b2f6b4fc90cdce0
Andrey Akhmichin <sepulkarium45@yahoo.com>
2019-11-04 00:31:52 +0500

    Fix mp5's bullet spread.

--- dlls/mp5.cpp
+++ dlls/mp5.cpp
@@ -151,9 +151,9 @@ void CMP5::PrimaryAttack()
 	Vector vecAiming = m_pPlayer->GetAutoaimVector( AUTOAIM_5DEGREES );
 	Vector vecDir;
 #ifdef CLIENT_DLL
-	if( !bIsMultiplayer() )
+	if( bIsMultiplayer() )
 #else
-	if( !g_pGameRules->IsMultiplayer() )
+	if( g_pGameRules->IsMultiplayer() )
 #endif
 	{
 		// optimized multiplayer. Widened to make it easier to hit a moving player
