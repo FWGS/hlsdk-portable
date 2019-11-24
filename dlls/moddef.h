666ff98ac703137865d9f786431be66521efffaa
Andrey Akhmichin <sepulkarium45@yahoo.com>
2019-11-25 01:34:35 +0500

    Define simple mods enumiration.

---
+++ dlls/moddef.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef _MODDEF
+#define _MODDEF
+
+enum
+{
+	MOD_VALVE,	// Half-Life
+	MOD_AOM,	// Afraid of Monsters
+	MOD_BIGLOLLY,	// Big Lolly
+	MOD_BSHIFT,	// Half-Life: Blue Shift
+	MOD_HALFSECRET,	// Half-Secret
+	MOD_HEVSUIT,	// Case Closed, Bloody Pizza: Vendetta, Borderlands
+	MOD_INDUCTION,	// Half-Life: Induction
+	MOD_KLEINER	// Kleiner's Adventures Demo
+	MOD_REDEMPT,	// Redemption/Absolute Redemption
+	MOD_SEWER_BETA,	// Sewer Beta
+	MOD_TOT		// Times of Troubles
+};
+
+#endif // _MODDEF
