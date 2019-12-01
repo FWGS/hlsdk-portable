c8db7ccf44208891f4a0421382581eca474c3690
Andrey Akhmichin <sepulkarium45@yahoo.com>
2019-12-01 15:15:31 +0500

    Remove useless python-specific changes.

--- dlls/weapons.cpp
+++ dlls/weapons.cpp
@@ -1629,9 +1629,3 @@ TYPEDESCRIPTION	CSatchel::m_SaveData[] =
 
 IMPLEMENT_SAVERESTORE( CSatchel, CBasePlayerWeapon )
 
-TYPEDESCRIPTION	CPython::m_SaveData[] =
-{
-	DEFINE_FIELD( CPython, m_flSoundDelay, FIELD_TIME ),
-};
-
-IMPLEMENT_SAVERESTORE( CPython, CBasePlayerWeapon )
