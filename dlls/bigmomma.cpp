54181adc8e401e3e3aba58d876639faa85fd19b0
Roman Chistokhodov <freeslave93@gmail.com>
2020-04-28 23:37:07 +0300

    Fix bigmomma node wait

--- dlls/bigmomma.cpp
+++ dlls/bigmomma.cpp
@@ -931,7 +931,7 @@ void CBigMomma::StartTask( Task_t *pTask )
 		TaskComplete();
 		break;
 	case TASK_WAIT_NODE:
-		m_flWait = gpGlobals->time + GetNodeDelay();
+		m_flWaitFinished = gpGlobals->time + GetNodeDelay();
 		if( m_hTargetEnt->pev->spawnflags & SF_INFOBM_WAIT )
 			ALERT( at_aiconsole, "BM: Wait at node %s forever\n", STRING( pev->netname ) );
 		else
@@ -1007,8 +1007,10 @@ void CBigMomma::RunTask( Task_t *pTask )
 			return;
 
 		if( gpGlobals->time > m_flWaitFinished )
+		{
 			TaskComplete();
-		ALERT( at_aiconsole, "BM: The WAIT is over!\n" );
+			ALERT( at_aiconsole, "BM: The WAIT is over!\n" );
+		}
 		break;
 	case TASK_PLAY_NODE_PRESEQUENCE:
 	case TASK_PLAY_NODE_SEQUENCE:
