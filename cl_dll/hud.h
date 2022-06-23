e5a1902661293ae91b53a3aabf24492937079304
Andrey Akhmichin <15944199+nekonomicon@users.noreply.github.com>
2022-06-23 06:53:20 +0500

    Make hud white.

--- cl_dll/hud.h
+++ cl_dll/hud.h
@@ -22,7 +22,7 @@
 #pragma once
 #if !defined(HUD_H)
 #define HUD_H
-#define RGB_YELLOWISH 0x00FFA000 //255,160,0
+#define RGB_YELLOWISH 0x00FFFFFF //255,255,255
 #define RGB_REDISH 0x00FF1010 //255,160,0
 #define RGB_GREENISH 0x0000A000 //0,160,0
 
