763af2905467b6b2df87a393866340821d4074dc
Roman Chistokhodov <freeslave93@gmail.com>
2022-07-18 16:13:58 +0300

    Add HANDGRENADE_DEPLOY_FIX macros for fixing the handgrenade deploy animation after finishing a throw (#275)

--- dlls/handgrenade.cpp
+++ dlls/handgrenade.cpp
@@ -173,7 +173,9 @@ void CHandGrenade::WeaponIdle( void )
 		// player "shoot" animation
 		m_pPlayer->SetAnimation( PLAYER_ATTACK1 );
 
+#if !HANDGRENADE_DEPLOY_FIX
 		m_flReleaseThrow = 0.0f;
+#endif
 		m_flStartThrow = 0.0f;
 		m_flNextPrimaryAttack = GetNextAttackDelay( 0.5f );
 		m_flTimeWeaponIdle = UTIL_WeaponTimeBase() + 0.5f;
