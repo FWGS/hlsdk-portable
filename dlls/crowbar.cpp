74f3ec7f61055e00b2f7602a8be849f4ce2aca0e
Roman Chistokhodov <freeslave93@gmail.com>
2022-07-19 12:32:51 +0300

    Fix crowbar idle animation time regardless of WEAPONS_ANIMATION_TIMES_FIX macro

--- dlls/crowbar.cpp
+++ dlls/crowbar.cpp
@@ -366,11 +366,7 @@ void CCrowbar::WeaponIdle( void )
 			if( flRand > 0.5f )
 			{
 				iAnim = CROWBAR_IDLE;
-#if WEAPONS_ANIMATION_TIMES_FIX
 				m_flTimeWeaponIdle = UTIL_WeaponTimeBase() + 70.0f / 25.0f;
-#else
-				m_flTimeWeaponIdle = UTIL_WeaponTimeBase() + 70.0f / 30.0f;
-#endif
 			}
 			else
 			{
