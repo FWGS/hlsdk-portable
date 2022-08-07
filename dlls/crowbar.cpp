e1aa21f1fcd3527c8c111d426049a0fa8fb6a258
Roman Chistokhodov <freeslave93@gmail.com>
2022-08-08 01:47:26 +0300

    Sync crowbar.cpp with master

--- dlls/crowbar.cpp
+++ dlls/crowbar.cpp
@@ -337,7 +337,7 @@ int CCrowbar::Swing( int fFirst )
 		SetThink( &CCrowbar::Smack );
 		pev->nextthink = gpGlobals->time + 0.2f;
 #endif
-#ifdef CROWBAR_DELAY_FIX
+#if CROWBAR_DELAY_FIX
 		m_flNextPrimaryAttack = UTIL_WeaponTimeBase() + 0.25f;
 #else
 		m_flNextPrimaryAttack = GetNextAttackDelay( 0.25f );
