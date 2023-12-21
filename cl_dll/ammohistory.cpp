0362b75fc10f6c238d62d0f8e0b8549aad5c4d72
Alibek Omarov <a1ba.omarov@gmail.com>
2023-12-21 04:12:25 +0300

    client: ammohistory: fix rendering history with bigger HUD sprites

--- cl_dll/ammohistory.cpp
+++ cl_dll/ammohistory.cpp
@@ -131,7 +131,7 @@ int HistoryResource::DrawAmmoHistory( float flTime )
 
 				// Draw the pic
 				int ypos = ScreenHeight - (AMMO_PICKUP_PICK_HEIGHT + (AMMO_PICKUP_GAP * i));
-				int xpos = ScreenWidth - 24;
+				int xpos = ScreenWidth - (rcPic.right - rcPic.left);
 				if( spr && *spr )    // weapon isn't loaded yet so just don't draw the pic
 				{
 					// the dll has to make sure it has sent info the weapons you need
