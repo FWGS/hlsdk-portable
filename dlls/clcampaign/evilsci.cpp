9bc467652f58a4cff37c74db06c2de50a5f57e53
Andrey Akhmichin <15944199+nekonomicon@users.noreply.github.com>
2024-01-02 10:45:38 +0500

    Hardcode evil scientist health.

--- dlls/clcampaign/evilsci.cpp
+++ dlls/clcampaign/evilsci.cpp
@@ -173,7 +173,7 @@ void CEvisci::Spawn()
 	pev->solid		= SOLID_SLIDEBOX;
 	pev->movetype		= MOVETYPE_STEP;
 	m_bloodColor		= BLOOD_COLOR_RED;
-	pev->health		= gSkillData.zombieHealth;
+	pev->health		= 800;
 	pev->view_ofs		= VEC_VIEW;// position of the eyes relative to monster's origin.
 	m_flFieldOfView		= 0.5;// indicates the width of this monster's forward view cone ( as a dotproduct result )
 	m_MonsterState		= MONSTERSTATE_NONE;
