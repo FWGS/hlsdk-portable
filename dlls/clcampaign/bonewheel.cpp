f192a47e90effe7d690fce69f7411f0d6bc6bee1
Andrey Akhmichin <15944199+nekonomicon@users.noreply.github.com>
2024-01-02 10:43:51 +0500

    Boost bonewheel health.

--- dlls/clcampaign/bonewheel.cpp
+++ dlls/clcampaign/bonewheel.cpp
@@ -170,7 +170,7 @@ void CBonewheel::Spawn()
 	pev->solid		= SOLID_SLIDEBOX;
 	pev->movetype		= MOVETYPE_STEP;
 	m_bloodColor		= DONT_BLEED;
-	pev->health		= 100;
+	pev->health		= 120;
 	pev->view_ofs		= VEC_VIEW;// position of the eyes relative to monster's origin.
 	m_flFieldOfView		= 0.5;// indicates the width of this monster's forward view cone ( as a dotproduct result )
 	m_MonsterState		= MONSTERSTATE_NONE;
