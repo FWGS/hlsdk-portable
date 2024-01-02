b6db8eb1aef56da83b599025d0867a5f90d4f94b
Andrey Akhmichin <15944199+nekonomicon@users.noreply.github.com>
2024-01-02 10:38:36 +0500

    Hardcode mario zombie health.

--- dlls/clcampaign/mariozombie.cpp
+++ dlls/clcampaign/mariozombie.cpp
@@ -268,7 +268,7 @@ void CMariozombie::Spawn()
 	pev->solid		= SOLID_SLIDEBOX;
 	pev->movetype		= MOVETYPE_STEP;
 	m_bloodColor		= BLOOD_COLOR_RED;
-	pev->health		= gSkillData.zombieHealth;
+	pev->health		= 150;
 	pev->view_ofs		= VEC_VIEW;// position of the eyes relative to monster's origin.
 	m_flFieldOfView		= 0.5;// indicates the width of this monster's forward view cone ( as a dotproduct result )
 	m_MonsterState		= MONSTERSTATE_NONE;
