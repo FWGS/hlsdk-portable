ba795b792793fc72e56c9bd197b6a6651bf363b1
Andrey Akhmichin <15944199+nekonomicon@users.noreply.github.com>
2024-01-02 10:35:40 +0500

    Boost megachav health.

--- dlls/clcampaign/megachav.cpp
+++ dlls/clcampaign/megachav.cpp
@@ -249,7 +249,7 @@ void CMegachar::Spawn()
 	pev->solid		= SOLID_SLIDEBOX;
 	pev->movetype		= MOVETYPE_STEP;
 	m_bloodColor		= BLOOD_COLOR_GREEN;
-	pev->health		= 1000;
+	pev->health		= 8000;
 	pev->view_ofs		= VEC_VIEW;// position of the eyes relative to monster's origin.
 	m_flFieldOfView		= 0.9;// indicates the width of this monster's forward view cone ( as a dotproduct result )
 	m_MonsterState		= MONSTERSTATE_NONE;
