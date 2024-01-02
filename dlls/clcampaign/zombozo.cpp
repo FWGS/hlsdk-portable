b84481986a2ff168bc35c86cd4d9d567e625f10a
Andrey Akhmichin <15944199+nekonomicon@users.noreply.github.com>
2024-01-02 10:39:32 +0500

    Boost zombozo health.

--- dlls/clcampaign/zombozo.cpp
+++ dlls/clcampaign/zombozo.cpp
@@ -245,7 +245,7 @@ void Czombozo::Spawn()
 	pev->solid		= SOLID_SLIDEBOX;
 	pev->movetype		= MOVETYPE_STEP;
 	m_bloodColor		= BLOOD_COLOR_GREEN;
-	pev->health		= 500;
+	pev->health		= 2000;
 	pev->view_ofs		= VEC_VIEW;// position of the eyes relative to monster's origin.
 	m_flFieldOfView		= 0.5;// indicates the width of this monster's forward view cone ( as a dotproduct result )
 	m_MonsterState		= MONSTERSTATE_NONE;
