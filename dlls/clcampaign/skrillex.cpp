6c9da9ab889c82620ac892c74bb89f836353f2a5
Andrey Akhmichin <15944199+nekonomicon@users.noreply.github.com>
2024-01-02 10:27:13 +0500

    Boost skrillex health.

--- dlls/clcampaign/skrillex.cpp
+++ dlls/clcampaign/skrillex.cpp
@@ -471,7 +471,7 @@ void CSkrillex::Spawn()
 	pev->movetype		= MOVETYPE_STEP;
 	m_bloodColor		= BLOOD_COLOR_RED;
 	pev->effects		= 0;
-	pev->health		= 2000;
+	pev->health		= 7600;
 	pev->view_ofs		= Vector( 0, 0, 64 );// position of the eyes relative to monster's origin.
 	m_flFieldOfView		= VIEW_FIELD_WIDE; // NOTE: we need a wide field of view so npc will notice player and say hello
 	m_MonsterState		= MONSTERSTATE_NONE;
