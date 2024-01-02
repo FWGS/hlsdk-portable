5551705f406a81eb2522767a9a22970aefd8452d
Andrey Akhmichin <15944199+nekonomicon@users.noreply.github.com>
2024-01-02 10:28:49 +0500

    Boost pink pantera health.

--- dlls/clcampaign/pink_panthera.cpp
+++ dlls/clcampaign/pink_panthera.cpp
@@ -249,7 +249,7 @@ void CPinkPanther::Spawn()
 	pev->solid		= SOLID_SLIDEBOX;
 	pev->movetype		= MOVETYPE_STEP;
 	m_bloodColor		= BLOOD_COLOR_RED;
-	pev->health		= 250;
+	pev->health		= 1100;
 	pev->view_ofs		= VEC_VIEW;// position of the eyes relative to monster's origin.
 	m_flFieldOfView		= 0.5;// indicates the width of this monster's forward view cone ( as a dotproduct result )
 	m_MonsterState		= MONSTERSTATE_NONE;
