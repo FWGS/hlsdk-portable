1f458edb0be871e729a475228ba025583bd4ee52
Andrey Akhmichin <15944199+nekonomicon@users.noreply.github.com>
2024-02-13 23:03:14 +0500

    Turn on weapon bobbing by default.

--- cl_dll/hud.cpp
+++ cl_dll/hud.cpp
@@ -380,7 +380,7 @@ void CHud::Init( void )
 	m_pCvarStealMouse = CVAR_CREATE( "hud_capturemouse", "1", FCVAR_ARCHIVE );
 	m_pCvarDraw = CVAR_CREATE( "hud_draw", "1", FCVAR_ARCHIVE );
 	cl_lw = gEngfuncs.pfnGetCvarPointer( "cl_lw" );
-	cl_viewbob = CVAR_CREATE( "cl_viewbob", "0", FCVAR_ARCHIVE );
+	cl_viewbob = CVAR_CREATE( "cl_viewbob", "1", FCVAR_ARCHIVE );
 
 	m_pSpriteList = NULL;
 
