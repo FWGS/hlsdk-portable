FWGS/hlsdk-portable

commit 253a3cb8d4873ced7090b13ef97de04fe8b48276

Better spawn spot randomization from HL25 update.

dlls/player.cpp
@@ -2794,6 +2794,8 @@ edict_t *EntSelectSpawnPoint( CBaseEntity *pPlayer )
 	CBaseEntity *pSpot;
 	edict_t *player;
 
+	int nNumRandomSpawnsToTry = 10;
+
 	player = pPlayer->edict();
 
 	// choose a info_player_deathmatch point
@@ -2808,9 +2810,18 @@ edict_t *EntSelectSpawnPoint( CBaseEntity *pPlayer )
 	}
 	else if( g_pGameRules->IsDeathmatch() )
 	{
+		if( !g_pLastSpawn )
+		{
+			nNumRandomSpawnsToTry = 0;
+			CBaseEntity* pEnt = 0;
+
+			while( ( pEnt = UTIL_FindEntityByClassname( 0, "info_player_deathmatch" )))
+				nNumRandomSpawnsToTry++;
+		}
+
 		pSpot = g_pLastSpawn;
 		// Randomize the start spot
-		for( int i = RANDOM_LONG( 1, 9 ); i > 0; i-- )
+		for( int i = RANDOM_LONG( 1, nNumRandomSpawnsToTry - 1 ); i > 0; i-- )
 			pSpot = UTIL_FindEntityByClassname( pSpot, "info_player_deathmatch" );
 		if( FNullEnt( pSpot ) )  // skip over the null point
 			pSpot = UTIL_FindEntityByClassname( pSpot, "info_player_deathmatch" );

commit 13086d3b5d7ab24b246160237ebc03bcc3ad0669

server: fix infinite loop while trying to look for spawn spot for deathmatch
Fixes: 253a3cb8d487 ("Better spawn spot randomization from HL25 update.")

dlls/player.cpp
@@ -2815,7 +2815,7 @@ edict_t *EntSelectSpawnPoint( CBaseEntity *pPlayer )
 			nNumRandomSpawnsToTry = 0;
 			CBaseEntity* pEnt = 0;
 
-			while( ( pEnt = UTIL_FindEntityByClassname( 0, "info_player_deathmatch" )))
+			while( ( pEnt = UTIL_FindEntityByClassname( pEnt, "info_player_deathmatch" )))
 				nNumRandomSpawnsToTry++;
 		}
 