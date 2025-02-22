78440742deae283ff26033a0c43651c3f5aebd7f
Elinsrc <bitkurky@gmail.com>
2025-02-22 13:25:20 +0500

    fix mp_bhopcap

--- dlls/player.cpp
+++ dlls/player.cpp
@@ -2916,7 +2916,7 @@ void CBasePlayer::Spawn( void )
 	g_engfuncs.pfnSetPhysicsKeyValue( edict(), "slj", "0" );
 	g_engfuncs.pfnSetPhysicsKeyValue( edict(), "hl", "1" );
 	g_engfuncs.pfnSetPhysicsKeyValue( edict(), "fr", "1" );
-	g_engfuncs.pfnSetPhysicsKeyValue( edict(), "bj", bhopcap.value ? "0" : "1" );
+	g_engfuncs.pfnSetPhysicsKeyValue( edict(), "bj", bhopcap.value ? "1" : "0" );
 
 	pev->fov = m_iFOV = 0;// init field of view.
 	m_iClientFOV = -1; // make sure fov reset is sent
