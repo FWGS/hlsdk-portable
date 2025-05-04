f16d2f5a50a892b98873ca00c8c2a03e5e4a6246
Andrey Akhmichin <15944199+nekonomicon@users.noreply.github.com>
2025-05-05 03:00:08 +0500

    Use safe string functions.

--- dlls/util.cpp
+++ dlls/util.cpp
@@ -698,8 +698,7 @@ CBaseEntity *UTIL_FollowGroupReference(CBaseEntity *pStartEntity, char* szGroupN
 		{
 			// recursive member-reference
 			// FIXME: we should probably check that i < MAX_ALIASNAME_LEN.
-			strncpy(szBuf,szMemberName,i);
-			szBuf[i] = 0;
+			strlcpy( szBuf, szMemberName, i + 1 );
 			szTail = &(szMemberName[i+1]);
 			szThisMember = szBuf;
 			break;
@@ -760,8 +759,7 @@ CBaseEntity *UTIL_FollowReference( CBaseEntity *pStartEntity, const char* szName
 		{
 			// yes, it looks like a reference through an info_group...
 			// FIXME: we should probably check that i < MAX_ALIASNAME_LEN.
-			strncpy(szRoot,szName,i);
-			szRoot[i] = 0;
+			strlcpy(szRoot, szName, i + 1 );
 			szMember = (char*)&szName[i+1];
 			//ALERT(at_console,"Following reference- group %s with member %s\n",szRoot,szMember);
 			pResult = UTIL_FollowGroupReference(pStartEntity, szRoot, szMember);
@@ -1374,8 +1372,7 @@ BOOL UTIL_IsMasterTriggered(string_t iszMaster, CBaseEntity *pActivator)
 					{
 						if (szMaster[j] == ')')
 						{
-							strncpy(szBuf, szMaster+i+1, (j-i)-1);
-							szBuf[(j-i)-1] = 0;
+							strlcpy( szBuf, szMaster + i + 1, j-i );
 							pActivator = UTIL_FindEntityByTargetname( NULL, szBuf );
 							found = true;
 							break;
@@ -1395,8 +1392,7 @@ BOOL UTIL_IsMasterTriggered(string_t iszMaster, CBaseEntity *pActivator)
 				return TRUE;
 			}
 
-			strncpy(szBuf, szMaster, i);
-			szBuf[i] = 0;
+			strlcpy( szBuf, szMaster, i + 1 );
 			pMaster = UTIL_FindEntityByTargetname( NULL, szBuf );
 		}
 
