a3200bee271423e0c8f93bab23ee4d5e889968db
Andrey Akhmichin <15944199+nekonomicon@users.noreply.github.com>
2025-07-27 01:01:39 +0500

    Update definitions of OSes and architectures.

--- dlls/bubblemod/BMOD_constants.h
+++ dlls/bubblemod/BMOD_constants.h
@@ -27,7 +27,7 @@
 
 #ifndef BMOD_CONSTANTS_H
 #define BMOD_CONSTANTS_H
-
+#include "build.h"
 // ---------------------------------------------------------------
 // Branch customizations
 // Information in the next three constants will be used for servers
@@ -52,34 +52,74 @@
 
 #define BMOD_VERSION			"2.2.3a"
 
-#if defined(_WIN32) && defined(_MSC_VER)
-#define BMOD_PLATFORM			"WIN32"
-#elif defined(_WIN32) && defined(__MINGW32_)
-#define BMOD_PLATFORM			"WIN32-MinGW"
-#elif defined(__ANDROID__)
+#if XASH_WIN32
+#define BMOD_PLATFORM			"Win32"
+#elif XASH_DOS4GW
+#define BMOD_PLATFORM			"DOS"
+#elif XASH_ANDROID
 #define BMOD_PLATFORM			"Android"
-#elif defined(__APPLE__) && ( defined(__arm__) || defined(__aarch64__) )
-#define BMOD_PLATFORM			"ios"
-#elif defined(__APPLE__)
+#elif XASH_IOS
+#define BMOD_PLATFORM			"iOS"
+#elif XASH_APPLE
 #define BMOD_PLATFORM			"Apple"
-#elif defined(__FreeBSD__)
+#elif XASH_FREEBSD
 #define BMOD_PLATFORM			"FreeBSD"
-#elif defined(__NetBSD__)
+#elif XASH_NETBSD
 #define BMOD_PLATFORM			"NetBSD"
-#elif defined(__OpenBSD__)
+#elif XASH_OPENBSD
 #define BMOD_PLATFORM			"OpenBSD"
-#else
+#elif XASH_LINUX
 #define BMOD_PLATFORM			"Linux"
-#endif
+#elif XASH_HAIKU
+#define BMOD_PLATFORM			"Haiku"
+#elif XASH_SERENITY
+#define BMOD_PLATFORM			"SerenityOS"
+#elif XASH_IRIX
+#define BMOD_PLATFORM			"IRIX"
+#elif XASH_NSWITCH
+#define BMOD_PLATFORM			"NSwitch"
+#elif XASH_PSVITA
+#define BMOD_PLATFORM			"PSVita"
+#elif XASH_WASI
+#define BMOD_PLATFORM			"WASI"
+#elif XASH_SUNOS
+#define BMOD_PLATFORM			"SunOS(Solaris)"
+#elif XASH_EMSCRIPTEN
+#define BMOD_PLATFORM			"Emscripten"
+#else
+#define BMOD_PLATFORM			"os-unknown"
+#endif 
 
-#if defined(__amd64__) || defined(_M_X64)
+#if XASH_AMD64
 #define BMOD_ARCH			"amd64"
-#elif defined(__aarch64__)
+#elif XASH_ARM >= 8
 #define BMOD_ARCH			"arm64"
-#elif defined(__i386__) || defined(_X86_) || defined(_M_IX86)
+#elif XASH_X86
 #define BMOD_ARCH			"i386"
-#elif defined(__arm__) || defined(_M_ARM)
+#elif XASH_ARM
 #define BMOD_ARCH			"arm"
+#elif XASH_RISCV 
+#if XASH_64BIT
+#define BMOD_ARCH			"riscv64"
+else
+#define BMOD_ARCH			"riscv32"
+#endif
+#elif XASH_MIPS
+#define BMOD_ARCH			"mips"
+#elif XASH_E2K
+#define BMOD_ARCH			"elbrus"
+#elif XASH_PPC
+#if XASH_64BIT
+#define BMOD_ARCH			"powerpc64"
+#else
+#define BMOD_ARCH			"powerpc"
+#endif
+#elif XASH_WASM
+#if XASH_64BIT
+#define BMOD_ARCH			"wasm64"
+#else
+#define BMOD_ARCH			"wasm"
+#endif
 #else
 #define BMOD_ARCH			"arch-unknown"
 #endif
