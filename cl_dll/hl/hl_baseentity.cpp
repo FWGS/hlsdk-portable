b1aea329cc7bce660e3f3db2a822a5a4ff430ecd
Andrey Akhmichin <15944199+nekonomicon@users.noreply.github.com>
2025-08-17 02:24:37 +0500

    client: fix undefined symbol.

--- cl_dll/hl/hl_baseentity.cpp
+++ cl_dll/hl/hl_baseentity.cpp
@@ -97,6 +97,7 @@ void CGrenade::Killed( entvars_t *, int ) { }
 void CGrenade::Spawn( void ) { }
 CGrenade *CGrenade::ShootTimed( entvars_t *pevOwner, Vector vecStart, Vector vecVelocity, float time ){ return 0; }
 CGrenade *CGrenade::ShootContact( entvars_t *pevOwner, Vector vecStart, Vector vecVelocity ){ return 0; }
+CGrenade *CGrenade::ClusterShootTimed( entvars_t *pevOwner, Vector vecStart, Vector vecVelocity, float time ){ return 0; }
 void CGrenade::DetonateUse( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value ){ }
 
 void UTIL_Remove( CBaseEntity *pEntity ){ }
