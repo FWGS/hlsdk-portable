38e13d5672abb5078c9985ddbb9b7c65784a7295
Alibek Omarov <a1ba.omarov@gmail.com>
2026-08-25 19:56:28 +0500

    client: deduplicate math functions

--- cl_dll/studio_util.h
+++ cl_dll/studio_util.h
@@ -28,10 +28,14 @@
 
 #define FDotProduct( a, b ) ( fabs( ( a[0] ) * ( b[0] ) ) + fabs( ( a[1] ) * ( b[1] ) ) + fabs( ( a[2] ) * ( b[2] ) ) )
 
+extern "C"
+{ // defined in pm_math.c
 void	AngleMatrix( const float *angles, float (*matrix)[4] );
 int	VectorCompare( const float *v1, const float *v2 );
 void	CrossProduct( const float *v1, const float *v2, float *cross );
 void	VectorTransform( const float *in1, float in2[3][4], float *out );
+}
+
 void	ConcatTransforms( float in1[3][4], float in2[3][4], float out[3][4] );
 void	MatrixCopy( float in[3][4], float out[3][4] );
 void	QuaternionMatrix( vec4_t quaternion, float (*matrix)[4] );
