76695846d2ba466dcf92c7654083fe0debfcb5c8
Roman Chistokhodov <freeslave93@gmail.com>
2026-09-13 02:44:37 +0300

    Fix spectator being shown as part of a team in the teamplay for newly connected players. Fix #526

--- dlls/player.cpp
+++ dlls/player.cpp
@@ -1491,7 +1491,7 @@ void CBasePlayer::StartObserver( Vector vecPosition, Vector vecViewAngle )
 	// Clear out the status bar
 	m_fInitHUD = TRUE;
 
-	pev->team = 0;
+	m_szTeamName[0] = '\0';
 	MESSAGE_BEGIN( MSG_ALL, gmsgTeamInfo );
 		WRITE_BYTE( ENTINDEX(edict()) );
 		WRITE_STRING( "" );
